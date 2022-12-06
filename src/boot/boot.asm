4aead9cfadc7f18cf48c55c2f457dbf2ad9f7578
Maniero <carlosmaniero@gmail.com>
2022-12-05 23:36:46 -0300

    create a basic structure to enter long mode

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,7 +1,48 @@
 global start
 
-section .text
+VGA_START equ 0xb8000
+VGA_END   equ VGA_START + 0x1000
+VGA_DEFAULT_COLOR equ 0x3000
+
 bits 32
-start:
-    mov dword [0xb8000], 0x2f4b2f4f
+
+    section .text
+clean_screen:
+    mov   eax, VGA_START
+.loop:
+    mov   word [eax], VGA_DEFAULT_COLOR + ' '
+    add   eax, 2
+    cmp   eax, VGA_END
+    jne   .loop
+    ret
+; Prints `ERR: ` and the given error code to screen and hangs.
+; parameter: error code (in ascii) in al
+print_error:
+    call    clean_screen
+    mov     dword [0xb8000], 0xCF72CF45
+    mov     dword [0xb8004], 0xCF3ACF72
+    mov     word  [0xb8008], 0xFC20
+    mov     ebx, 0xb800a
+    mov     ah, 0xFC
+.loop:
+    lodsb
+    cmp     al, 0
+    je      .hang
+
+    mov     word [ebx], ax
+
+    add     ebx, 2
+    jmp     .loop
+.hang:
     hlt
+start:
+    lea esi, [ERROR_CODE_NO_CPUID - 0xc0000000]
+    jmp print_error
+
+    section .data
+ERROR_CODE_NO_CPUID db "0x00010000 NoCPUID", 0
+
+    section .bss
+stack_bottom:
+    resb 64
+stack_top:
